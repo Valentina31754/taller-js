fbf5db609d6beadf000381bb8f4d17d04e47baea
Valentina31754 <113189149+Valentina31754@users.noreply.github.com>
2022-12-02 11:28:42 -0500

    Add files via upload

---
+++ Taller#1JS/ejercicios.js
@@ -0,0 +1,77 @@
+/*
+pagar nomina a un empleado que recibe x sueldo ,
+calcular cuanto se le debe descontar  de 
+salud 4% y pencion 5%  cuanto seria su salario neto a recibir
+*/
+/*
+var sueldo= parseInt( prompt("dijite el sueldo"));
+var salud= sueldo* 4/100;//0.04
+var pencion =sueldo* 5/100;
+var salarioNeto= sueldo - salud - pencion;
+
+document.write("su salario es :"+sueldo+"<br>");
+document.write("El dto de su salud es:"+salud+"<br>");
+document.write("El dto de su pencion es :"+pencion+"<br>");
+document.write("su salario a pagar es :"+salarioNeto+"<br>");
+*/
+
+/*punto 2
+Aplica la formula a =(b*h)/2
+para calcular el area  debe un triangulo  donde sus dimenciones base y altura
+se deben pedir al usuarioque las dijite.
+*/
+/*
+var base =parseInt(prompt("Dijite base del triangulo"));
+var altura= parseInt(prompt("dijite la altura del tringulo"));
+var area=(base*altura)/2;
+document.write("la base del triangulo es :"+base+"<br>");
+document.write("la altura del triangulo es :"+altura+"<br>");
+document.write("el area del triangulo es :"+area+"<br>");
+*/
+
+/*punto3
+var Num1=parseFloat(prompt("dijite el primer numero"));
+var Num2=parseFloat(prompt("dijite el segundo numero"));
+var suma=Num1+Num2;
+var resta=Num1-Num2;
+var multiplicacion=Num1*Num2;
+var divicion=Num1/Num2;
+var modulo=Num1%Num2;
+document.write("la suma de los numeros es :"+suma+"<br>");
+document.write("la rest de los numeros es :"+resta+"<br>");
+document.write("la multiplicacion de los numeros es :"+multiplicacion+"<br>");
+document.write("la divicion de los numeros es :"+divicion+"<br>");
+document.write("el modulo de los numeros es :"+modulo+"<br>");
+*/
+
+/*punto5
+
+var precioProducto = parseFloat(prompt("ingrese el precio del producto;"));
+var iva = precioProducto*0.19;
+var total = precioProducto+iva;
+
+document.write("en precio del su producto es:"+precioProducto+"<br>");
+document.write("en precio del su producto con iva es :"+total+"<br>");
+document.write("el iva que se le agrega es de:"+iva+"<br>");
+
+
+
+/*punto7*/
+var nombre=prompt("dijite el nombre del alumno");
+var Nota1=parseFloat(prompt("ingrese la primera nota"));
+var Nota2=parseFloat(prompt("ingrese la segunda nota"));
+var Nota3=parseFloat(prompt("ingrese la primera nota"));
+var promedio=(Nota1+Nota2+Nota3)/3;
+
+document.write("en nombre del alumno es:"+nombre+"<br>");
+document.write("su promedio de la nota es:"+promedio+"<br>");
+
+
+
+
+
+
+
+
+
+
